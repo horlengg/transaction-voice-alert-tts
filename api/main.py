8a21b322cf5ad6309b198e54c7df3f25f1cd0c73
Leng <horleng123@gmail.com>
2026-09-02 09:42:55 +0700

    add apple-app-site-association endpoint for testing universal link

--- api/main.py
+++ api/main.py
@@ -1,4 +1,7 @@
+import os
+
 from fastapi import Depends, FastAPI
+from fastapi.responses import JSONResponse
 from app.routers import speech,audio
 from app.auth import verify_api_key
 from dotenv import load_dotenv
@@ -17,3 +20,39 @@ app.include_router(
 @app.get("/health", tags=["health"])
 def health():
     return {"status": "ok" }
+
+
+
+APP_ID = os.environ["APP_ID"]
+
+# Explore Universal Domain
+AASA_DATA = {
+    "applinks": {
+        "apps": [],
+        "details": [
+            {
+                "appIDs": [APP_ID],
+                "components": [
+                    {
+                        "/": "/*",
+                        "comment": "Matches all paths"
+                    }
+                ]
+            }
+        ]
+    }
+}
+
+# 1. Primary endpoint required by Apple
+@app.api_route(
+    "/.well-known/apple-app-site-association", 
+    methods=["GET", "HEAD"], 
+    response_class=JSONResponse
+)
+@app.api_route(
+    "/apple-app-site-association", 
+    methods=["GET", "HEAD"], 
+    response_class=JSONResponse
+)
+async def get_aasa():
+    return JSONResponse(content=AASA_DATA, media_type="application/json")
